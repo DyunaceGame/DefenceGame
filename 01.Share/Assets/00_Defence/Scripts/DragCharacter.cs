5196a8e5eae6345a98971b86f34fa90cb62235d1
Dyunace <tmddn5924@gmail.com>
2023-08-08 20:03:27 +0900

    세부변경

--- 01.Share/Assets/00_Defence/Scripts/DragCharacter.cs
+++ 01.Share/Assets/00_Defence/Scripts/DragCharacter.cs
@@ -15,24 +15,26 @@ public class DragCharacter : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            //RaycastHit2D hit = Physics2D.Raycast(mainCam.ViewportToWorldPoint(Input.mousePosition), transform.forward, Mathf.Infinity);
             RaycastHit2D hit = Physics2D.Raycast(mainCam.ScreenToWorldPoint(Input.mousePosition), transform.forward, Mathf.Infinity);
 
             // 대상이 있다면 실행
             if (hit)
             {
-                print("hit");
-
                 if (hit.collider.CompareTag("Character"))
                 {
+                    // 입력받은 화면 위치를 디스플레이 위치로 변경
+                    // 벡터 포지션 값을 0~1 까지의 값으로 변경함
                     Vector2 targetPos = mainCam.ScreenToViewportPoint(Input.mousePosition);
 
+                    // 디스플레이 위치를 이동 가능한 지점까지 제한
                     targetPos = new(Mathf.Clamp(targetPos.x, 0, 1), Mathf.Clamp(targetPos.y, 0, 1));
 
-                    // 화면 밖으로 나가면 안됌.
-                    // 포지션 값은 마우스 위치를 기준으로 받음
-                    // 
-                    
+                    // 변경된 위치를 월드 좌표로 계산
+                    targetPos = mainCam.ViewportToWorldPoint(targetPos);
+
+                    print(targetPos);
+
+                    // 좌표 적용
                     hit.transform.position = targetPos;
                 }
             }
